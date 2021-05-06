4432c7a3de8af1376b3ec00f81af999835c40d37
LeagueNightEveryNight <59737655+LeagueNightEveryNight@users.noreply.github.com>
2021-05-06 19:27:04 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({id, index, flipCard, onClick}) => {
   return (
     <div className={`card ${flipCard ? "flipped" : ""}`} key={index}>
       <div className="inside">
-        <div className="front"><img className="card" id={id} src={`/JPEG/${id}.jpg`} alt=""></img></div>
+        <div className="front"><img className="card" id={id} src={`./JPEG/${id}.jpg`} alt=""></img></div>
         <div className="back">
           <img id={id} onClick={onClick} src={picture} alt="back of a card" className="cardBack"></img>
         </div>
